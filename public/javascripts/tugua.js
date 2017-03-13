b37211772d0e0f53723324347ce7f26ec1597f96
adug3 <m_fan@geishatokyo.com>
2017-03-13 15:54:51 +0900

    add page switcher

--- public/javascripts/tugua.js
+++ public/javascripts/tugua.js
@@ -1,9 +1,126 @@
 $(document).ready(function () {
-  var s = Snap("#svg");
-  var bigCircle = s.circle(150, 150, 100);
-  var text = s.text(50, 50, 'Start').attr({ "font-size": "36" });
-  Snap.load("/images/arrow.svg", function (svg) {
-    console.log(this);
-    console.log(svg);
-  }, $("#svg"));
+  var animator = new Animator();
+  animator.page1();
 });
+
+function Animator(){
+  this.s = Snap("#svg");
+  this.screenWidth = $("#svg").width();
+  this.screenHeight = $("#svg").height();
+}
+
+function remove(element){
+  var dfd = $.Deferred();
+  var currentY = element.getBBox().y;
+  element.animate({
+    "y": currentY - 1000,
+    "opacity": 0
+  },500, function (){
+    element.remove();
+    dfd.resolve();
+  });
+  return dfd.promise();
+}
+
+Animator.prototype.page1 = function (){
+  var self = this;
+  var text = self.s.text(self.screenWidth/2,self.screenHeight/2,'Start').attr({
+      "font-size": "36",
+      "text-anchor": "middle",
+      "opacity": 0
+    });
+  text.animate({
+    "opacity": 1
+  }, 1000, function (){
+    var arrow = self.s.image("/images/icon_bounce_arrow_100.gif", self.screenWidth/2-25, self.screenHeight-50, 50, 50);
+    var touched = false;
+    var nextPage = function(){
+      if (touched) return;
+      touched = true;
+      self.s.untouchmove(nextPage);
+      $.when(
+        remove(text),
+        remove(arrow)
+      ).done(
+        function (){
+          self.page2();
+        }
+      );
+    };
+    self.s.touchmove(nextPage);
+  });
+};
+
+Animator.prototype.page2 = function (){
+  var self = this;
+  var text = self.s.text(self.screenWidth/2,self.screenHeight/2,'Page2').attr({
+      "font-size": "36",
+      "text-anchor": "middle",
+      "opacity": 0
+    });
+  text.animate({
+    "opacity": 1
+  }, 1000, function (){
+    var arrow = self.s.image("/images/icon_bounce_arrow_100.gif", self.screenWidth/2-25, self.screenHeight-50, 50, 50);
+    var touched = false;
+    var nextPage = function(){
+      if (touched) return;
+      touched = true;
+      self.s.untouchmove(nextPage);
+      $.when(
+        remove(text),
+        remove(arrow)
+      ).done(
+        function (){
+          self.page3();
+        }
+      );
+    };
+    self.s.touchmove(nextPage);
+  });
+};
+
+Animator.prototype.page3 = function (){
+  var dfd = $.Deferred();
+  var self = this;
+  var text = self.s.text(self.screenWidth/2,self.screenHeight/2,'Page3').attr({
+      "font-size": "36",
+      "text-anchor": "middle",
+      "opacity": 0
+    });
+  text.animate({
+    "opacity": 1
+  }, 1000, function (){
+    var arrow = self.s.image("/images/icon_bounce_arrow_100.gif", self.screenWidth/2-25, self.screenHeight-50, 50, 50);
+    var touched = false;
+    var nextPage = function(){
+      if (touched) return;
+      touched = true;
+      self.s.untouchmove(nextPage);
+      $.when(
+        remove(text),
+        remove(arrow)
+      ).done(
+        function (){
+          self.page4();
+        }
+      );
+    };
+    self.s.touchmove(nextPage);
+  });
+};
+
+Animator.prototype.page4 = function (){
+  var dfd = $.Deferred();
+  var self = this;
+  var text = self.s.text(self.screenWidth/2,self.screenHeight/2,'Finish').attr({
+      "font-size": "36",
+      "text-anchor": "middle",
+      "opacity": 0
+    });
+  text.animate({
+    "opacity": 1
+  }, 1000, function (){
+    //var arrow = self.s.image("/images/icon_bounce_arrow_100.gif", self.screenWidth/2-25, self.screenHeight-50, 50, 50);
+  });
+};
